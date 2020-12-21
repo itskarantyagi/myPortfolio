70761f7092cb5ca16dda7e12bca9fbf0662e4eed
itskarantyagi <karantyagi80@gmail.com>
2020-12-21 21:05:44 +0530

    nav-bar double click fixed

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 function about_div()
 {
 console.log(document.getElementById("about-div"))
-if ((document.getElementById("about-div").style.display === "none") || (document.getElementById("about-div").style.display === "block"))
+if (document.getElementById("about-div").style.display === "block")
     {
-    document.getElementById("about-div").style.display = "block";
+    document.getElementById("about-div").style.display = "none";
     document.getElementById("design-div").style.display = "none";
     document.getElementById("development-div").style.display = "none";
     document.getElementById("jukebox-div").style.display = "none";
     }
     else 
     {
-        document.getElementById("about-div").style.display = "none";
+        document.getElementById("about-div").style.display = "block";
         document.getElementById("design-div").style.display = "none";
         document.getElementById("development-div").style.display = "none";
         document.getElementById("jukebox-div").style.display = "none";
@@ -20,17 +20,17 @@ if ((document.getElementById("about-div").style.display === "none") || (document
 function design_div()
 {
 console.log(document.getElementById("design-div"))
-if ((document.getElementById("design-div").style.display === "none") || (document.getElementById("design-div").style.display === "block"))
+if (document.getElementById("design-div").style.display === "block")
     {
     document.getElementById("about-div").style.display = "none";
-    document.getElementById("design-div").style.display = "block";
+    document.getElementById("design-div").style.display = "none";
     document.getElementById("development-div").style.display = "none";
     document.getElementById("jukebox-div").style.display = "none";
     }
     else 
     {
         document.getElementById("about-div").style.display = "none";
-        document.getElementById("design-div").style.display = "none";
+        document.getElementById("design-div").style.display = "block";
         document.getElementById("development-div").style.display = "none";
         document.getElementById("jukebox-div").style.display = "none";
         }
@@ -40,18 +40,18 @@ if ((document.getElementById("design-div").style.display === "none") || (documen
 function my_work_div()
       {
       console.log(document.getElementById("development-div"))
-      if ((document.getElementById("development-div").style.display === "none") || (document.getElementById("development-div").style.display === "block"))
+      if (document.getElementById("development-div").style.display === "block")
           {
           document.getElementById("about-div").style.display = "none";
           document.getElementById("design-div").style.display = "none";
-          document.getElementById("development-div").style.display = "block";
+          document.getElementById("development-div").style.display = "none";
           document.getElementById("jukebox-div").style.display = "none";
           }
           else 
           {
               document.getElementById("about-div").style.display = "none";
               document.getElementById("design-div").style.display = "none";
-              document.getElementById("development-div").style.display = "none";
+              document.getElementById("development-div").style.display = "block";
               document.getElementById("jukebox-div").style.display = "none";
               }
             }
@@ -60,19 +60,19 @@ function my_work_div()
 function jukebox_div()
             {
             console.log(document.getElementById("jukebox-div"))
-            if ((document.getElementById("jukebox-div").style.display === "none") || (document.getElementById("jukebox-div").style.display === "block"))
+            if (document.getElementById("jukebox-div").style.display === "block")
                 {
                 document.getElementById("about-div").style.display = "none";
                 document.getElementById("design-div").style.display = "none";
                 document.getElementById("development-div").style.display = "none";
-                document.getElementById("jukebox-div").style.display = "block";
+                document.getElementById("jukebox-div").style.display = "none";
                 }
                 else 
                 {
                     document.getElementById("about-div").style.display = "none";
                     document.getElementById("design-div").style.display = "none";
                     document.getElementById("development-div").style.display = "none";
-                    document.getElementById("jukebox-div").style.display = "none";
+                    document.getElementById("jukebox-div").style.display = "block";
                     }
                   }
 
