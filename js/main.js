dd5c237943316fe8a5b0391d77cbde305cdc0c7d
Karan Tyagi <karan@Mac-1518.lan>
2025-12-28 19:24:26 -0500

    remove design section

--- js/main.js
+++ js/main.js
@@ -1,43 +1,22 @@
 function about_div()
 {
         document.getElementById("about-div").style.display = "block";
-        document.getElementById("design-div").style.display = "none";
         document.getElementById("development-div").style.display = "none";
         document.getElementById("jukebox-div").style.display = "none";
 
         document.getElementById("about").style.color="orangered";
-        document.getElementById("design").style.color="white";
         document.getElementById("my-work").style.color="white";
         document.getElementById("jukebox").style.color="white";
         }
       
 
-function design_div()
-{
-
-        document.getElementById("about-div").style.display = "none";
-        document.getElementById("design-div").style.display = "block";
-        document.getElementById("development-div").style.display = "none";
-        document.getElementById("jukebox-div").style.display = "none";
-        
-        
-        document.getElementById("about").style.color="white";
-        document.getElementById("design").style.color="orangered";
-        document.getElementById("my-work").style.color="white";
-        document.getElementById("jukebox").style.color="white";
-       
-      }
-
-
 function my_work_div()
       {
               document.getElementById("about-div").style.display = "none";
-              document.getElementById("design-div").style.display = "none";
               document.getElementById("development-div").style.display = "block";
               document.getElementById("jukebox-div").style.display = "none";
 
               document.getElementById("about").style.color="white";
-              document.getElementById("design").style.color="white";
               document.getElementById("my-work").style.color="orangered";
               document.getElementById("jukebox").style.color="white";
               
@@ -47,12 +26,10 @@ function my_work_div()
 function jukebox_div()
             {
                     document.getElementById("about-div").style.display = "none";
-                    document.getElementById("design-div").style.display = "none";
                     document.getElementById("development-div").style.display = "none";
                     document.getElementById("jukebox-div").style.display = "block";
 
                     document.getElementById("about").style.color="white";
-                    document.getElementById("design").style.color="white";
                     document.getElementById("my-work").style.color="white";
                     document.getElementById("jukebox").style.color="orangered";
 
