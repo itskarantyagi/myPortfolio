3490a1e33b9597d67bbff8609bc209bb2f4d6f25
Karan Tyagi <karan@Mac-1518.lan>
2025-12-28 19:41:10 -0500

    cleanup

--- js/main.js
+++ js/main.js
@@ -1,40 +1,32 @@
-function about_div()
-{
-        document.getElementById("about-div").style.display = "block";
-        document.getElementById("development-div").style.display = "none";
-        document.getElementById("jukebox-div").style.display = "none";
+function about_div() {
+  document.getElementById("about-div").style.display = "block";
+  document.getElementById("development-div").style.display = "none";
+  document.getElementById("jukebox-div").style.display = "none";
 
-        document.getElementById("about").style.color="orangered";
-        document.getElementById("my-work").style.color="white";
-        document.getElementById("jukebox").style.color="white";
-        }
-      
+  document.getElementById("about").style.color = "orangered";
+  document.getElementById("my-work").style.color = "white";
+  document.getElementById("jukebox").style.color = "white";
+}
 
-function my_work_div()
-      {
-              document.getElementById("about-div").style.display = "none";
-              document.getElementById("development-div").style.display = "block";
-              document.getElementById("jukebox-div").style.display = "none";
+function my_work_div() {
+  document.getElementById("about-div").style.display = "none";
+  document.getElementById("development-div").style.display = "block";
+  document.getElementById("jukebox-div").style.display = "none";
 
-              document.getElementById("about").style.color="white";
-              document.getElementById("my-work").style.color="orangered";
-              document.getElementById("jukebox").style.color="white";
-              
-            }
-            
-            
-function jukebox_div()
-            {
-                    document.getElementById("about-div").style.display = "none";
-                    document.getElementById("development-div").style.display = "none";
-                    document.getElementById("jukebox-div").style.display = "block";
+  document.getElementById("about").style.color = "white";
+  document.getElementById("my-work").style.color = "orangered";
+  document.getElementById("jukebox").style.color = "white";
+}
 
-                    document.getElementById("about").style.color="white";
-                    document.getElementById("my-work").style.color="white";
-                    document.getElementById("jukebox").style.color="orangered";
+function jukebox_div() {
+  document.getElementById("about-div").style.display = "none";
+  document.getElementById("development-div").style.display = "none";
+  document.getElementById("jukebox-div").style.display = "block";
 
-                    }
-                  
+  document.getElementById("about").style.color = "white";
+  document.getElementById("my-work").style.color = "white";
+  document.getElementById("jukebox").style.color = "orangered";
+}
 
 function nav_collapse() {
   var x = document.getElementById("Topnav");
@@ -43,14 +35,14 @@ function nav_collapse() {
   } else {
     x.className = "nav";
   }
-} 
+}
 
-function play(audioid){
-       var audio = document.getElementById(audioid);
-       audio.play();
-                 }
+function play(audioid) {
+  var audio = document.getElementById(audioid);
+  audio.play();
+}
 
-      function stop(audioid){
-       var audio = document.getElementById(audioid);
-       audio.pause();
-                 }
+function stop(audioid) {
+  var audio = document.getElementById(audioid);
+  audio.pause();
+}
